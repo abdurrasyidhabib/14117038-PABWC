4d8b428f7f863d1358b8305d9d21632d29998dcc
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-10-08 13:59:59 +0700

    menyelesaikan latihan 2

---
+++ latihan2.php
@@ -0,0 +1,44 @@
+<?php
+// kalkulator latihan2
+
+$a = 15;
+$b = 5;
+
+echo "Bilangan 1 = $a <br>";
+echo "Bilangan 2 = $b <br>";
+
+echo "<br/> Penjumlahan <br/>";
+$c = $a + $b;
+echo "operator = +";
+echo "<br>";
+echo "hasilnya : $a + $b = $c";
+echo "<br>";
+
+
+echo "<br/> Pengurangan <br/>";
+$d = $a - $b;
+echo "operator = -";
+echo "<br>";
+echo "hasilnya : $a - $b = $d";
+echo "<br>";
+
+echo "<br/> Perkalian <br/>";
+$e = $a * $b;
+echo "operator = *";
+echo "<br>";
+echo "hasilnya : $a * $b = $e";
+echo "<br>";
+
+echo "<br/> Pembagian <br/>";
+$f = $a / $b;
+echo "operator = /";
+echo "<br>";
+echo "hasilnya : $a / $b = $f";
+echo "<br>";
+
+echo "<br/> Modulus <br/>";
+$g = $a % $b;
+echo "operator = %";
+echo "<br>";
+echo "hasilnya : $a % $b = $g";
+echo "<br>";

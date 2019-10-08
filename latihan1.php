e3dba5daefa9f18e573794a0e07f7670bfb21845
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-10-08 13:50:29 +0700

    menyelesaikan latihan1, pakai styling sederhana

---
+++ latihan1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Latihan1</title>
+</head>
+<style>
+  body {
+    font-size: 28px;
+    text-align: center;
+  }
+
+  p {
+    margin-top: 100px;
+  }
+
+  b {
+    color: darkblue;
+  }
+</style>
+
+<body>
+  <?php
+  $nama = "Habib Abdurrasyid kece";
+  echo "<p>$nama berkata: <br> <b> \"I'm a web developer!\"<b> </p>"
+  ?>
+</body>
+
+</html>
